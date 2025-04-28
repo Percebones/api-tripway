05605c22697f386b4ec28363b60d54a820f42433
Thiago Percebon Silva <141276166+Percebones@users.noreply.github.com>
2025-04-28 20:32:10 -0300

    first commit

---
+++ src/main/java/br/com/tripway/api/TripwayApplication.java
@@ -0,0 +1,13 @@
+package br.com.tripway.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tripway/api/TripwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tripway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
